3e1248e5283e81248afd5a2160bc8da5cb9aa837
twikssi <limpbiski15@gmail.com>
2020-07-08 20:21:30 +0300

    Create first homework

---
+++ homeWork_01/ex2_Algorithm/AndryBorisevich/src/main/java/by/jrr/Andryborisevich/App.java
@@ -0,0 +1,8 @@
+package by.jrr.Andryborisevich;
+
+public class App {
+    public static void main(String[] args) {
+        int counter = 0;
+        System.out.println("Hi, mom" + counter + 1);
+    }
+}
