4678b9d9a94f626e70f47bea8254c3f914c8df14
Zakhar <zaxar.cuprelyow@yandex.ru>
2020-07-09 19:06:56 +0300

    Create 1 homework

---
+++ homeWork_01/ex1_ObjectsAroundUs/Kuprelyow/src/main/java/by/jrr/Kuprelyow/App.java
@@ -0,0 +1,7 @@
+package by.jrr.Kuprelyow;
+
+public class App {
+    public static void main(String[] args) {
+        Objects nw = new Objects();
+    }
+}
---
+++ homeWork_01/ex1_ObjectsAroundUs/Kuprelyow/src/main/java/by/jrr/Kuprelyow/Objects.java
@@ -0,0 +1,106 @@
+package by.jrr.Kuprelyow;
+
+import java.security.PublicKey;
+
+public class Objects {
+
+    public Objects(){
+        PC();
+        Book();
+        Food();
+        TV();
+        Comix();
+        Phone();
+        Pencil();
+        Music();
+        Car();
+        Bike();
+    }
+
+    public void PC(){
+        System.out.println("1)Computer");
+        System.out.println("Condition:");
+        System.out.println("    Included/turned off");
+        System.out.println("    Code it/You don't code");
+        System.out.println("Behavior:");
+        System.out.println("    Included - work for a PC");
+        System.out.println("    Turned off - don't work for a PC");
+        System.out.println("Behavior:");
+        System.out.println("    Code it - learning JAVA");
+        System.out.println("    You don't code - you don't learn JAVA");
+    }
+    public void Book(){
+        System.out.println("2)Book");
+        System.out.println("Condiyion:");
+        System.out.println("    Read/Not read");
+        System.out.println("Behavior:");
+        System.out.println("    Read - Learned a new");
+        System.out.println("    Not read - did not learn new");
+    }
+    public void Food(){
+        System.out.println("3)Food");
+        System.out.println("Condiyion:");
+        System.out.println("    Ready/Not");
+        System.out.println("Behavior:");
+        System.out.println("    Ready to eat");
+        System.out.println("    No-I didn't eat it");
+    }
+    public void TV(){
+        System.out.println("4)TV");
+        System.out.println("Condition:");
+        System.out.println("    Included/turned off");
+        System.out.println("Behavior:");
+        System.out.println("    Included -  watch TV");
+        System.out.println("    Turned off - don't watch TV");
+
+    }
+    public void Comix(){
+        System.out.println("5)Comix");
+        System.out.println("Condiyion:");
+        System.out.println("    Read/Not read");
+        System.out.println("Behavior:");
+        System.out.println("    Read - Learned a new");
+        System.out.println("    Not read - did not learn new");
+    }
+    public void Phone(){
+        System.out.println("6)Phone");
+        System.out.println("Condiyion:");
+        System.out.println("    On/Off");
+        System.out.println("Behavior:");
+        System.out.println("    On - talk on the phone");
+        System.out.println("    Off - don't talk on the phone");
+    }
+    public void Pencil(){
+        System.out.println("7)Pencil");
+        System.out.println("Condiyion:");
+        System.out.println("    Yes/no");
+        System.out.println("Behavior:");
+        System.out.println("    Yes - draw");
+        System.out.println("    No - not draw");
+    }
+    public void Music(){
+        System.out.println("8)Music");
+        System.out.println("Condiyion:");
+        System.out.println("    Yes/no");
+        System.out.println("Behavior:");
+        System.out.println("    Yes - fun");
+        System.out.println("    No - not fun");
+    }
+    public void Car(){
+        System.out.println("9)Car");
+        System.out.println("Condiyion:");
+        System.out.println("    Correct/Not correct");
+        System.out.println("Behavior:");
+        System.out.println("    Correct - going");
+        System.out.println("    Not correct - not going");
+    }
+    public void Bike(){
+        System.out.println("10)Bike");
+        System.out.println("Condiyion:");
+        System.out.println("    Yes/No");
+        System.out.println("Behavior:");
+        System.out.println("    Yes - going");
+        System.out.println("    No - not going");
+    }
+
+}
