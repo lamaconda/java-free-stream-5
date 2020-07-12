3d30145042b06b9e34e894a05781b22625203ed3
Siarhei_Paliakou <serpolak@gmail.com>
2020-07-12 11:19:06 +0300

    Homework1_Task2

---
+++ homeWork_01/ex2_Algorithm/Sergei_Poliakou/src/main/java/by/serp/serpolak/Translation.java
@@ -0,0 +1,37 @@
+package by.serp.serpolak;
+
+public class Translation {
+    public static void main(String[] args) {
+
+
+        readIt();
+        writeOutWords();
+        understandIt();
+        useADictionary();
+        translateIt();
+
+    }
+
+
+    public static void readIt() {
+        System.out.println("Read the text");
+    }
+
+    public static void writeOutWords() {
+        System.out.println("Write out words that are no known");
+    }
+
+    public static void understandIt() {
+        System.out.println("Try to understand the meaning of these words");
+    }
+
+    public static void useADictionary() {
+        System.out.println("Use a dictionary to translate these words");
+    }
+
+    public static void translateIt() {
+        System.out.println("Translate the text");
+    }
+
+
+}
