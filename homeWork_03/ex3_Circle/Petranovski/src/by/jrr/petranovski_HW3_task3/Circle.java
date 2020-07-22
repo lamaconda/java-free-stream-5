dd071df2777cba81ad21a7a22bfb969478d2d25b
VladimirPetranovski <desantluninec@mail.ru>
2020-07-22 14:49:23 +0300

    check my Petranovski_HW3_Task3

---
+++ homeWork_03/ex3_Circle/Petranovski/src/by/jrr/petranovski_HW3_task3/Circle.java
@@ -0,0 +1,9 @@
+package by.jrr.petranovski_HW3_task3;
+
+public class Circle {
+    public double radius;
+    public double calculateArea() {
+        final double PI = 3.14;
+        return PI * radius * radius;
+    }
+}
