3d6a6206247db6328e8133d5ec27bc60afe43600
Alexander Sheremet <13az111o@gmail.com>
2020-07-22 15:33:21 +0300

    sheremet_hw3_circle

---
+++ homeWork_03/ex3_Circle/sheremet/src/main/java/by/jrr/sheremet/task3/App.java
@@ -0,0 +1,8 @@
+package main.java.by.jrr.sheremet.task3;
+
+public class App {
+    public static void main(String[] args) {
+        Circle circle = new Circle(3.56);
+        System.out.println("Area = " + circle.calculateArea());
+    }
+}
